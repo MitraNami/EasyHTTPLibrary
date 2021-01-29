bda8562a56100bed2c357f26911ecdb6fbb4ad91
Mitra Nami <a.mxnami@gmail.com>
2021-01-28 23:19:08 -0500

    HTTP POST request method

--- HTTP-ES7-fetch/app.js
+++ HTTP-ES7-fetch/app.js
@@ -2,8 +2,22 @@ const http = new EasyHTTP();
 
 
 // HTTP GET request: Get all the users
-http.get('https://jsonplaceholder.typicode.com/users')
-  .then(users => console.log(users))
+// http.get('https://jsonplaceholder.typicode.com/users')
+//   .then(users => console.log(users))
+//   .catch(e => console.log(e));
+
+
+const newUser = {
+  name: 'Mitra Nami',
+  email: 'a.mxnami@gmail.com',
+  addres: {
+    city: 'Kingston',
+    street: 'Johnson'
+  }
+};
+
+
+// HTTP POST request: Create a new user
+http.post('https://jsonplaceholder.typicode.com/users', newUser)
+  .then(newUser => console.log(newUser))
   .catch(e => console.log(e));
-
-
--- HTTP-ES7-fetch/easyhttp3.js
+++ HTTP-ES7-fetch/easyhttp3.js
@@ -20,6 +20,18 @@ class EasyHTTP {
   }
 
 
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify(data)
+    });
+    if (!response.ok) {
+      throw Error(`HTTP Error: status ${response.status}`);
+    }
+    const newUser = await response.json();
+    return newUser;
+  }
 
 
 }
